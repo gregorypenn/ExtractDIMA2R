3814ca8fd61a727e2f6b75e99a7fa6285f5fe370
Gregory Penn <gregory@crediblecorvid.com>
2023-12-16 09:24:41 -0700

    Added DIMA_tables, a vector of all DIMA table names.

--- R/data.R
+++ R/data.R
@@ -3,3 +3,9 @@
 #' @format ## `CFO_tables`
 #' A character vector with names of 17 DIMA tables.
 "CFO_tables"
+
+#' Names of all DIMA tables
+#'
+#' @format ## `DIMA_tables`
+#' A character vector with names of 152 DIMA tables.
+"DIMA_tables"
