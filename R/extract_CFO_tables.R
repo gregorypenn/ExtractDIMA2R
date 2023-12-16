54634f256061ba04b68288671b6c13dbd4c3ade0
Gregory Penn <gregory@crediblecorvid.com>
2023-12-16 09:13:24 -0700

    Updated documentation.

--- R/extract_CFO_tables.R
+++ R/extract_CFO_tables.R
@@ -1,11 +1,16 @@
 #' Extract DIMA tables for the BLM's Carlsbad Field Office
 #'
+#' @details
+#' The names of DIMA tables extracted are stored in the character vector
+#' `CFO_tables`, which is exported from this package.
+#'
 #' @param dima A filepath to a DIMA database.
 #'
 #' @return A list of dataframes, each with the name and contents of a DIMA table.
 #' @export
 extract_CFO_tables <- function (dima) {
-  query <- purrr::map(CFO_tables, query_table_SQL)
+  tables <- CFO_tables
+  query <- purrr::map(tables, query_table_SQL)
   names(query) <- tables
   return(query_dima(dima, query))
 }
